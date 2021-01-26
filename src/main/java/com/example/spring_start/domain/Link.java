e5f525b1940e8d64c637ae5fc173ef0128a67162
msys <ruba.badawi98@gmail.com>
2021-01-26 15:24:01 +0200

    Updated application properties with some basic h2 & datasource setup

--- src/main/java/com/example/spring_start/domain/Link.java
+++ src/main/java/com/example/spring_start/domain/Link.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
