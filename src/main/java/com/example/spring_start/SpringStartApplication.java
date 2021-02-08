7606791ddc5b037149f4947300f5f45a2887d05b
msys <ruba.badawi98@gmail.com>
2021-02-08 14:11:56 +0200

    added account register and profile

--- src/main/java/com/example/spring_start/SpringStartApplication.java
+++ src/main/java/com/example/spring_start/SpringStartApplication.java
@@ -15,6 +15,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SpringStartApplication {
@@ -28,6 +29,13 @@ public class SpringStartApplication {
     @Bean
     PrettyTime prettyTime() {
         return new PrettyTime();
+    }
+
+    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+    // TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
     }
 
 }
