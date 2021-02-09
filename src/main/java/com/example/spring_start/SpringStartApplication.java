9499931112f50c6f809bea581787d78306c5cf6f
msys <ruba.badawi98@gmail.com>
2021-02-09 10:39:14 +0200

    transactional

--- src/main/java/com/example/spring_start/SpringStartApplication.java
+++ src/main/java/com/example/spring_start/SpringStartApplication.java
@@ -1,23 +1,17 @@
 package com.example.spring_start;
 
 
-import com.example.spring_start.domain.Comment;
-import com.example.spring_start.domain.Link;
-import com.example.spring_start.repository.CommentRepository;
-import com.example.spring_start.repository.LinkRepository;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringStartApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringStartApplication.class);
--- src/main/java/com/example/spring_start/service/UserService.java
+++ src/main/java/com/example/spring_start/service/UserService.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -25,5 +28,14 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    @Transactional
+    public void saveUsers(User... users) {
+        for(User user : users) {
+            logger.info("Saving User: " + user.getEmail());
+            userRepository.save(user);
+        }
+    }
+
 
 }
